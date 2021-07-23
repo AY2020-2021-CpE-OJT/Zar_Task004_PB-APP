82dd63f282965d65b696f50dba2887b0c8d217c5
Teyzar <thomasandrewzaragoza@gmail.com>
2021-07-23 20:00:47 +0800

    added delete & update API request

--- lib/API.dart
+++ lib/API.dart
@@ -1,19 +1,23 @@
+
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 import 'dart:convert';
 import './Model/data_model.dart';
+import 'Auth/token.dart';
 
 Future<Contacts> createContact(
     String firstname, String lastname, List<dynamic> phonenumbers) async {
   Uri url = Uri.http('firstappdeployment.herokuapp.com', '/router/create');
+  
   final res = await http.post(url,
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization' : 'Bearer $token'
       },
       body: jsonEncode(<dynamic, dynamic>{
         'firstname': firstname,
         'lastname': lastname,
-        'phonenumbers': phonenumbers
+        'phonenumbers': phonenumbers,
       }));
   if (res.statusCode == 201) {
     final String resposeString = res.body;
@@ -22,14 +26,36 @@ Future<Contacts> createContact(
     throw Exception('Failed to create Contact.');
   }
 }
-
 Future<Contacts> deleteContact(String id) async {
   Uri url = Uri.http('firstappdeployment.herokuapp.com', '/router/delete/$id');
   final res = await http.delete(url, headers: <String, String>{
     'Content-Type': 'application/json; charset=UTF-8',
+    'Authorization' : 'Bearer $token'
   });
+  
   if (res.statusCode == 200) {
     return Contacts.fromJson(jsonDecode(res.body));
+  } else {
+    throw Exception('Failed to create Contact.');
+  }
+}
+
+Future<Contacts> editContact(String id, String firstname, String lastname, List<dynamic> phonenumbers) async {
+  Uri url = Uri.http('firstappdeployment.herokuapp.com', '/router/edit/$id');
+  
+  final res = await http.put(url,
+      headers: <String, String>{
+        'Authorization' : 'Bearer $token'
+      },
+      body: jsonEncode(<dynamic, dynamic>{
+        'firstname': firstname,
+        'lastname': lastname,
+        'phonenumbers': phonenumbers,
+      }));
+
+  if (res.statusCode == 200) {
+    final String resposeString = res.body;
+    return Contacts.fromJson(jsonDecode(resposeString));
   } else {
     throw Exception('Failed to create Contact.');
   }
