f7930b592a6f6559252f3881bee80608b87866b4
Teyzar <thomasandrewzaragoza@gmail.com>
2021-07-27 19:43:01 +0800

    added visibility

--- lib/Contact/editContact.dart
+++ lib/Contact/editContact.dart
@@ -4,6 +4,8 @@ import 'package:flutter_mobileapp/ContactList.dart';
 import 'package:flutter_mobileapp/Model/data_model.dart';
 import '../API.dart';
 import './newContact.dart';
+import 'package:toast/toast.dart';
+import './displayContact.dart';
 
 
 class EditContact extends StatefulWidget {
@@ -15,6 +17,7 @@ class EditContact extends StatefulWidget {
   _EditContactState createState() => _EditContactState();
 }
 
+
 class _EditContactState extends State<EditContact> {
   TextEditingController editfname = TextEditingController();
   TextEditingController editlname = TextEditingController();
@@ -23,29 +26,44 @@ class _EditContactState extends State<EditContact> {
   final List<Todo> _list = <Todo>[];
   int _num = 0;
 
+
+  bool _isVisible = true;
+  bool _isVisible2 = false;
+      void showToast() {
+	
+      setState(() {
+	
+        _isVisible = !_isVisible;
+        _isVisible2 = !_isVisible2;
+	
+      });
+	
+    }
+
+
   
   @override
   void initState() {
     super.initState();
-
-
+    
       editfname = TextEditingController(text: widget.pfirstname);
       editlname = TextEditingController(text: widget.plastname);
-      editphonenum = <TextEditingController>[TextEditingController(text: widget.phonenum.toString().replaceAll("[", "").replaceAll("]", ""))];
-      
   }
   
   void addContact() {
     List<String> phonenum = <String>[];
+
     
     for (int i = 0; i < _num; i++) {
       phonenum.add(editphonenum[i].text);
     }
+    if (_num == 0) {
+      editContact(widget.passid, editfname.text, editlname.text, widget.phonenum);
+    } else {
     setState(() {
-    _list.insert(0, Todo(editlname.text, editfname.text, phonenum));
-    
-    _futureContacts = editContact(widget.passid, editfname.text, editlname.text, phonenum);
-    });
+      editContact(widget.passid, editfname.text, editlname.text, phonenum);
+      });
+    }
                   
     for (int i = 0; i < _num; i++) {
       editlname.clear();
@@ -53,44 +71,43 @@ class _EditContactState extends State<EditContact> {
       editphonenum[i].clear();
       }
 
-    final toast = SnackBar(
-      content: Text("Edit Succesful",
-          style: TextStyle(fontSize: 17, color: Colors.white)),
-      backgroundColor: Colors.blue[40],
-    );
-    ScaffoldMessenger.of(context).showSnackBar(toast);
+    String x = "Edit Succesful";
+    Toast.show(x, context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM, backgroundColor: Colors.grey, textColor: Colors.black);
+
+    
   }
 
+  void addeditphone() {
+    setState(() {
+      _num+=widget.phonenum.length;
+      widget.phonenum.forEach((var str) {
+        editphonenum.insert(0, TextEditingController(text: str));
+      });
+    });
+  }
+  void hidenumbers() {
+    setState(() {
+      _num=0;
+      widget.phonenum.forEach((var str) {
+        editphonenum.insert(0, TextEditingController(text: str));
+      });
+    });
+  }
   void addphone() {
     setState(() {
       _num++;
-      editphonenum.insert(0, TextEditingController());
-    });
+        editphonenum.insert(0, TextEditingController());
+      });
   }
-  void subtractphone() {
+  void subtracteditphone() {
     setState(() {
       _num--;
-      editphonenum.insert(0, TextEditingController());
+     widget.phonenum.forEach((var str) {
+        editphonenum.insert(0, TextEditingController(text: str));
+      });
     });
   }
-  Widget getList() {
-  List<dynamic> list = widget.phonenum;
-  ListView myList = new ListView.builder(
-    itemCount: list.length,
-    itemBuilder: (context, index) {
-    return new ListTile(
-    title: TextField(
-          controller: editfname,
-          decoration: InputDecoration(
-            contentPadding: const EdgeInsets.all(20),
-            icon: Icon(Icons.person, color: Colors.black),
-            border: OutlineInputBorder()),
-          keyboardType: TextInputType.name,
-        ),
-    );
-  });
-  return myList;
-}
+
   @override
   Widget build(BuildContext context) {
     
@@ -123,16 +140,20 @@ class _EditContactState extends State<EditContact> {
           },
       child: Text("SAVE", style: TextStyle(color: Colors.black, fontSize: 20))),
       body: Column(children: [
-        CircleAvatar(
+        Padding( padding: EdgeInsets.all(0),
+        child: CircleAvatar(
           child: FittedBox(
-            fit: BoxFit.cover,
+            fit: BoxFit.fill,
             child: Image(
-              image: AssetImage('lib/Contact/user.png'),
+              width: 180,
+              fit: BoxFit.fill,
+              alignment: Alignment.center,
+              image: AssetImage('lib/Contact/edit.png'),
             ),
           ),
-          backgroundColor: Colors.blue,
-          radius: 70,
-        ),
+          backgroundColor: Colors.grey[50],
+          radius: 80,
+        )),
         SizedBox(height: 10),
         Flexible(
           child: ListView.builder(
@@ -145,6 +166,7 @@ class _EditContactState extends State<EditContact> {
                   contentPadding: const EdgeInsets.all(20),
                   icon: Icon(Icons.person, color: Colors.black),
                   border: OutlineInputBorder()),
+                  style: TextStyle(color: Colors.black, fontSize: 17, fontStyle: FontStyle.italic),
                 keyboardType: TextInputType.name,
               ),
             );
@@ -163,42 +185,79 @@ class _EditContactState extends State<EditContact> {
                   contentPadding: const EdgeInsets.all(20),
                   icon: SizedBox(width: 24),
                   border: OutlineInputBorder()),
+                  style: TextStyle(color: Colors.black, fontSize: 17, fontStyle: FontStyle.italic),
                 keyboardType: TextInputType.name,
               ),
             );
           },
           itemCount: 1,
         )),
-        SizedBox(height: 5),
+        SizedBox(height: 0),
         Flexible(
-            child: ListView.builder(
-          shrinkWrap: true,
-          itemBuilder: (context, i) {
-            return ListTile(
-              title: TextField(
-                controller: editphonenum[i],
-                decoration: InputDecoration(
-                  icon: Icon(Icons.phone_android),
+          child: ListView.builder(
+            itemCount: _num,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: TextField(
+                  controller: editphonenum[index],
+                  decoration: InputDecoration(
+                  icon: Icon(Icons.phone_android, color: Colors.black),
                   border: UnderlineInputBorder(),
                   suffixIcon: IconButton(
-                    onPressed: subtractphone,
+                    onPressed: subtracteditphone,
                     icon: Text("-", style: TextStyle(fontSize: 50, height: 0.75, color: Colors.red)),
-                  )
-                  ),
-                maxLines: 1,
-                keyboardType: TextInputType.number,
+                  )),
+                  style: TextStyle(color: Colors.black, fontSize: 17, fontStyle: FontStyle.italic),
+                  maxLength: 11,
+                  keyboardType: TextInputType.number,
+                ));
+            })),
+            Visibility(
+              visible: _isVisible,
+              child: TextButton(
+              onPressed: () {
+                if (_num != widget.phonenum.length) {
+                  addeditphone();
+                  _isVisible = false;
+                  _isVisible2 = true;
+                } else if (_num == widget.phonenum.length){
+                  String msg = "No other existing numbers";
+                    Toast.show(msg, context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
+                }
+              },
+              child: Text( 
+              " SHOW NUMBERS",
+              style: TextStyle(fontSize: 15, color: Colors.black, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold, height: 1, decoration: TextDecoration.underline
               ),
-            );
-          },
-          itemCount: _num,
-        )),
-        TextButton(
-          onPressed: addphone,
-          child: Text( 
-          " ➕ add phone",
-          style: TextStyle(fontSize: 15, color: Colors.black), 
-        )),
-      ].toList()),
+            )
+          )),
+          Visibility(
+              visible: _isVisible2,
+              child: TextButton(
+              onPressed: () {
+                if (_num == widget.phonenum.length) {
+                  hidenumbers();
+                  _isVisible2 = false;
+                  _isVisible = true;
+                }
+              },
+              child: Text( 
+              " HIDE NUMBERS",
+              style: TextStyle(fontSize: 15, color: Colors.black, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold, height: 1, decoration: TextDecoration.underline
+              ),
+            )
+          )),
+          TextButton(
+              onPressed: () {
+                if (_num != widget.phonenum.length -1) {addphone();}else {}
+              },
+              child: Text( 
+              " ➕ add phone",
+              style: TextStyle(fontSize: 15, color: Colors.black
+              ), 
+            )
+          ),
+      ]),
     );
   }
 }
